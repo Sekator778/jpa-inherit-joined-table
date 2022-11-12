d31ae1e4c29010fc140e9a1a4508873cf0e40c3f
Sekator <sekator778@gmail.com>
2022-11-12 15:32:26 +0100

    Initial commit

---
+++ src/main/java/com/sekator/jpainheritjoinedtable/JpaInheritJoinedTableApplication.java
@@ -0,0 +1,13 @@
+package com.sekator.jpainheritjoinedtable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaInheritJoinedTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaInheritJoinedTableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sekator/jpainheritjoinedtable/JpaInheritJoinedTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sekator.jpainheritjoinedtable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaInheritJoinedTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
